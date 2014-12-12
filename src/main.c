848e1a8cfc3495527eef1403241141091823fc0f
xkz123 <654989771@qq.com>
2014-12-12 21:25:13 +0800

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,19 +1,4 @@
-/*
-** 2001 September 15
-**
-** The author disclaims copyright to this source code.  In place of
-** a legal notice, here is a blessing:
-**
-**    May you do good and not evil.
-**    May you find forgiveness for yourself and forgive others.
-**    May you share freely, never taking more than you give.
-**
-*************************************************************************
-** Main file for the SQLite library.  The routines in this file
-** implement the programmer interface to the library.  Routines in
-** other files are for internal use by SQLite and should not be
-** accessed by users of the library.
-*/
+
 #include "sqliteInt.h"
 
 #ifdef SQLITE_ENABLE_FTS3
