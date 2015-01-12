5349bd6f796eb40a28c75e56704aafdd805527dd
michaelyangxiao <476911470@qq.com>
2015-01-12 12:47:55 +0800

    Update update.c

--- src/update.c
+++ src/update.c
@@ -18,7 +18,7 @@
 /* Forward declaration */
 static void updateVirtualTable(
   Parse *pParse,       /* The parsing context解析指针 */
-  SrcList *pSrc,       /* The virtual table to be modified 要修改的虚拟表*/
+  SrcList *pSrc,       /* The virtual table to be modified 要修改的虚拟表 */
   Table *pTab,         /* The virtual table 虚拟表*/
   ExprList *pChanges,  /* The columns to change in the UPDATE statement  处理UPDATE语句中的列变化 */
   Expr *pRowidExpr,    /* Expression used to recompute the rowid   定义的一个表达式，用来验证*/
