804825c4ecff1e806489fbb03c3b5eb9ad8e8192
Sundaylf <407906774@qq.com>
2015-12-23 15:35:47 +0800

    孙留芳
    第一次提交，第七次修改

--- src/memjournal.c
+++ src/memjournal.c
@@ -14,68 +14,69 @@
 ** The in-memory rollback journal is used to journal transactions for
 ** ":memory:" databases and when the journal_mode=MEMORY pragma is used.
 */
-/*此文件中的代码用于实现内存DB日志。在内存中的回滚事务用于存储事务汇报，
+/*此文件中的代码用于实现内存DB回卷日志。在内存中的回滚事务用于存储事务汇报，
 当journal_mode=MEMORY，说明内存编译使用中。*/
-/*这个文件的代码用于实现实现内存的回滚日志，内存回滚日志被用来汇报日志对数据库的记忆
-当journal_mode=MEMORY 指示被使用*/
-
 #include "sqliteInt.h"
 
 /* Forward references to internal structures */
-/*提出对内部结构的引用*/
-/*内存结构体变量声明*/
-typedef struct MemJournal MemJournal;/*结构体类型的变量MemJournal*/
-typedef struct FilePoint FilePoint;/*结构体类型的变量FilePoint*/
-typedef struct FileChunk FileChunk;/*结构体类型的变量FileChunk*/
+/*对于内部结构的引用*/
+typedef struct MemJournal MemJournal;
+typedef struct FilePoint FilePoint;
+typedef struct FileChunk FileChunk;
 
 /* Space to hold the rollback journal is allocated in increments of
 ** this many bytes.
-**存放回滚日志的空间被分配在这么多字节的内存增量中。
+**存放回卷日志的空间被分配在这么多字节的内存增量中。
 ** The size chosen is a little less than a power of two.  That way,
 ** the FileChunk object will have a size that almost exactly fills
 ** a power-of-two allocation.  This mimimizes wasted space in power-of-two
 ** memory allocators.
 */
-/*输入的大小小于2的幂。这样，FileChunk对象会填充内存分配大小至2的幂数，这样就能将空间浪费降至最小。*/
-/*选择的大小是小于2的幂。这样，FileChunk对象会填充内存分配大小至2的幂数，这样就能将空间浪费降至最小。*/
+/*输入的大小小于2的幂。这样，FileChunk对象会填充内存分配大小至2的幂数。
+**这样就能将空间浪费降至最小。
+*/
 #define JOURNAL_CHUNKSIZE ((int)(1024-sizeof(FileChunk*)))
 
 /* Macro to find the minimum of two numeric values.
 */
-/*宏MIN能找到两个数中的最小值*/
+/*宏能找到两个数中的最小值*/
 #ifndef MIN
 # define MIN(x,y) ((x)<(y)?(x):(y))
 #endif
-
+ 
 /*
 ** The rollback journal is composed of a linked list of these structures.
 */
-/*回滚日志是一个由这些结构体组成的链表*/
+/*回卷日志是由这些结构的链表组成的。*/
 struct FileChunk {
   FileChunk *pNext;               /* Next chunk in the journal *//*指向下一个节点*/
-  u8 zChunk[JOURNAL_CHUNKSIZE];   /* Content of this chunk *//*节点存放的内容*/ /*无符号的字符型数组zChunk[]为节点存放的内容 */
+  u8 zChunk[JOURNAL_CHUNKSIZE];   /* Content of this chunk *//*这个节点的内容*/
 };
 
 /*
 ** An instance of this object serves as a cursor into the rollback journal.
 ** The cursor can be either for reading or writing.
 */
-/*此对象的一个实例作为一个游标到恢复日志。游标可以用于读或写。*/
+/*这个对象的一个实例作为一个游标到回卷日志。这个游标可以进行读和写。*/
+/*游标（cursor）是系统为用户开设的一个数据缓冲区，存放SQL语句的执行结果。
+**每个游标区都有一个名字,用户可以用SQL语句逐一从游标中获取记录，并赋给主变量，
+**交由主语言进一步处理。
+*/
 struct FilePoint {
   sqlite3_int64 iOffset;          /* Offset from the beginning of the file *//*文件开头的偏移量*/
-  FileChunk *pChunk;              /* Specific chunk into which cursor points *//*游标指到其中的特定内存块*/
+  FileChunk *pChunk;              /* Specific chunk into which cursor points *//*游标指到的内存块*/
 };
 
 /*
 ** This subclass is a subclass of sqlite3_file.  Each open memory-journal
 ** is an instance of this class.
 */
-/*这是sqlite3_file的一个子类的子类。 每个打开的内存日志是此类的实例。*/
+/*这是sqlite3_file的一个子类。每一个打开的内存日志是这个类的一个实例。*/
 struct MemJournal {
-  sqlite3_io_methods *pMethod;    /* Parent class. MUST BE FIRST 指向父类的指针。*/
-  FileChunk *pFirst;              /* Head of in-memory chunk-list 指向内存块列表的头*/
-  FilePoint endpoint;             /* Pointer to the end of the file 指向文件的末尾*/
-  FilePoint readpoint;            /* Pointer to the end of the last xRead() 指向xRead尾部的指针*/
+  sqlite3_io_methods *pMethod;    /* Parent class. MUST BE FIRST 。*//*指向父类的指针*/
+  FileChunk *pFirst;              /* Head of in-memory chunk-list *//*指向内存块列表的头*/
+  FilePoint endpoint;             /* Pointer to the end of the file *//*指向文件的末尾*/
+  FilePoint readpoint;            /* Pointer to the end of the last xRead() *//*到最后xread()末端指针*/
 };
 
 /*
@@ -84,10 +85,10 @@ struct MemJournal {
 */
 /*从内存中读取数据的日志文件。 这是一个实现sqlite3_vfs.xRead方法。*/
 static int memjrnlRead(
-  sqlite3_file *pJfd,    /* The journal file from which to read 日志文件的阅读*/
-  void *zBuf,            /* Put the results here 将结果放到这里*/
-  int iAmt,              /* Number of bytes to read 读取的字节数*/
-  sqlite_int64 iOfst     /* Begin reading at this offset 从这个偏移量开始阅读*/
+  sqlite3_file *pJfd,    /* The journal file from which to read *//*日志文件阅读*/
+  void *zBuf,            /* Put the results here *//*存放结果*/
+  int iAmt,              /* Number of bytes to read *//*读取的字节数*/
+  sqlite_int64 iOfst     /* Begin reading at this offset *//*从这个偏移量开始阅读*/
 ){
   MemJournal *p = (MemJournal *)pJfd;
   u8 *zOut = zBuf;
@@ -96,7 +97,7 @@ static int memjrnlRead(
   FileChunk *pChunk;
 
   /* SQLite never tries to read past the end of a rollback journal file */
-  /*数据库不会试图读取之前回滚日志文件的最后事务。*/
+  /*SQLite不会试图读取之前回滚日志文件的最后事务。*/
   assert( iOfst+iAmt<=p->endpoint.iOffset );
 
   if( p->readpoint.iOffset!=iOfst || iOfst==0 ){
@@ -127,13 +128,13 @@ static int memjrnlRead(
 }
 
 /*
-** Write data to the file.数据写入文件。
+** Write data to the file.将数据写入文件
 */
 static int memjrnlWrite(
-  sqlite3_file *pJfd,    /* The journal file into which to write 日志文件写入*/
-  const void *zBuf,      /* Take data to be written from here 将zBuf里的数据写入*/
-  int iAmt,              /* Number of bytes to write 写入的字节数*/
-  sqlite_int64 iOfst     /* Begin writing at this offset into the file 从这个偏移量开始写入*/
+  sqlite3_file *pJfd,    /* The journal file into which to write *//*写入日志文件*/
+  const void *zBuf,      /* Take data to be written from here *//*将zBuf中的数据写入*/
+  int iAmt,              /* Number of bytes to write *//*写入数据的大小*/
+  sqlite_int64 iOfst     /* Begin writing at this offset into the file *//*从这个偏移量开始写入*/
 ){
   MemJournal *p = (MemJournal *)pJfd;
   int nWrite = iAmt;
@@ -142,7 +143,7 @@ static int memjrnlWrite(
   /* An in-memory journal file should only ever be appended to. Random
   ** access writes are not required by sqlite.
   */
-  /*内存中的日志文件只能被追加。随机存取不需要写入数据库。*/
+  /*内存中的日志文件只能被追加，sqlite不需要写入数据。*/
   assert( iOfst==p->endpoint.iOffset );
   UNUSED_PARAMETER(iOfst);
 
@@ -152,7 +153,7 @@ static int memjrnlWrite(
     int iSpace = MIN(nWrite, JOURNAL_CHUNKSIZE - iChunkOffset);
 
     if( iChunkOffset==0 ){
-      /* New chunk is required to extend the file. 扩展文件需要新的内存块。*/
+      /* New chunk is required to extend the file. 需要新的块来扩展这个文件*/
       FileChunk *pNew = sqlite3_malloc(sizeof(FileChunk));
       if( !pNew ){
         return SQLITE_IOERR_NOMEM;
@@ -178,7 +179,7 @@ static int memjrnlWrite(
 }
 
 /*
-** Truncate the file.截断文件。
+** Truncate the file.截断该文件。
 */
 static int memjrnlTruncate(sqlite3_file *pJfd, sqlite_int64 size){
   MemJournal *p = (MemJournal *)pJfd;
@@ -196,7 +197,7 @@ static int memjrnlTruncate(sqlite3_file *pJfd, sqlite_int64 size){
 }
 
 /*
-** Close the file.关闭该文件。
+** Close the file.关闭文件
 */
 static int memjrnlClose(sqlite3_file *pJfd){
   memjrnlTruncate(pJfd, 0);
@@ -205,15 +206,16 @@ static int memjrnlClose(sqlite3_file *pJfd){
 
 
 /*
-** Sync the file.
-**同步文件。
+** Sync the file.同步文件
+**
 ** Syncing an in-memory journal is a no-op.  And, in fact, this routine
 ** is never called in a working implementation.  This implementation
 ** exists purely as a contingency, in case some malfunction in some other
 ** part of SQLite causes Sync to be called by mistake.
 */
-/* 同步内存中的日志是一个空操作。而事实上，在一个工作执行中是不会调用这个函数文件的。
-这个操作作为应急而存在，以防误称一些SQLite其他部分的故障同步。*/
+/*同步内存中的日志是一个空操作。事实上，在一个工作执行中是不会调用这个函数文件的。
+**这个操作作为应急而存在，以防一些SQLite其他部分的同步故障。
+*/
 static int memjrnlSync(sqlite3_file *NotUsed, int NotUsed2){
   UNUSED_PARAMETER2(NotUsed, NotUsed2);
   return SQLITE_OK;
@@ -230,22 +232,22 @@ static int memjrnlFileSize(sqlite3_file *pJfd, sqlite_int64 *pSize){
 
 /*
 ** Table of methods for MemJournal sqlite3_file object.
-**MemJournal sqlite3_file对象的方法表。
+** MemJournal sqlite3_file对象的方法表。
 */
 static const struct sqlite3_io_methods MemJournalMethods = {
-   1,                /* iVersion 版本*/
-  memjrnlClose,     /* xClose  关闭文件*/
-  memjrnlRead,      /* xRead 读文件*/
-  memjrnlWrite,     /* xWrite 写文件*/
-  memjrnlTruncate,  /* xTruncate截断文件 */
-  memjrnlSync,      /* xSync 文件同步*/
-  memjrnlFileSize,  /* xFileSize 文件大小 */
-  0,                /* xLock 加锁*/
-  0,                /* xUnlock 不加锁*/
-  0,                /* xCheckReservedLock检查排斥锁 */
-  0,                /* xFileControl 文件控制*/
-  0,                /* xSectorSize扇区大小 */
-  0,                /* xDeviceCharacteristics 设备特性 */
+  1,                /* iVersion */
+  memjrnlClose,     /* xClose */
+  memjrnlRead,      /* xRead */
+  memjrnlWrite,     /* xWrite */
+  memjrnlTruncate,  /* xTruncate */
+  memjrnlSync,      /* xSync */
+  memjrnlFileSize,  /* xFileSize */
+  0,                /* xLock */
+  0,                /* xUnlock */
+  0,                /* xCheckReservedLock */
+  0,                /* xFileControl */
+  0,                /* xSectorSize */
+  0,                /* xDeviceCharacteristics */
   0,                /* xShmMap */
   0,                /* xShmLock */
   0,                /* xShmBarrier */
@@ -265,7 +267,7 @@ void sqlite3MemJournalOpen(sqlite3_file *pJfd){
 
 /*
 ** Return true if the file-handle passed as an argument is 
-** an in-memory journal 
+** an in-memory journal.
 **如果文件句柄作为参数传递的是内存中的日志，返回true
 */
 int sqlite3IsMemJournal(sqlite3_file *pJfd){
--- src/mutex.c
+++ src/mutex.c
@@ -1,4 +1,4 @@
-﻿/*
+/*
 ** 2007 August 14
 **
 ** The author disclaims copyright to this source code.  In place of
@@ -10,7 +10,7 @@
 **
 *************************************************************************
 ** This file contains the C functions that implement mutexes.
-**这个文档包含实现互斥锁的C 函数
+**这个文档包含实现互斥锁的C函数
 **
 ** This file contains code that is common across all mutex implementations.
 **这个文档包含共同所有互斥锁的代码实现。
@@ -18,17 +18,12 @@
 #include "sqliteInt.h"
 
 #if defined(SQLITE_DEBUG) && !defined(SQLITE_MUTEX_OMIT)
-/////*用于调试目的,当互斥锁子系统进行初始化的和未初始化时记录,
-////以便如果有试图分配一个互斥锁而系统未初始化时我们可以assert()。
-////如果定义了SQLITE_DEBUG且没有定义SQLITE_MUTEX_OMIT，则对mutexIsInit（为静态常量）初始化为零。
-*/
 /*
 ** For debugging purposes, record when the mutex subsystem is initialized
 ** and uninitialized so that we can assert() if there is an attempt to
 ** allocate a mutex while the system is uninitialized.
-**用于调试目的,记录时,互斥锁子系统初始化的和未初始化,
-**以便我们可以断言()如果有试图分配一个互斥而系统未初始化。
-**
+**用于调试目的,记录互斥锁子系统初始化的和未初始化的,
+**当系统未初始化时，如果试图分配一个互斥，那么我们可以使用assert()
 **
 */
 static SQLITE_WSD int mutexIsInit = 0;
@@ -37,7 +32,7 @@ static SQLITE_WSD int mutexIsInit = 0;
 
 #ifndef SQLITE_MUTEX_OMIT
 /*
-** Initialize the mutex system.如果没有定义SQLITE_MUTEX_OMIT就初始化互斥系统
+** Initialize the mutex system.初始化互斥系统
 */
 int sqlite3MutexInit(void){ 
   int rc = SQLITE_OK;
@@ -46,33 +41,25 @@ int sqlite3MutexInit(void){
     ** install a mutex implementation via sqlite3_config() prior to 
     ** sqlite3_initialize() being called. This block copies pointers to
     ** the default implementation into the sqlite3GlobalConfig structure.
-    **如果xMutexAlloc 函数没设置，
+    **如果xMutexAlloc方法没设置，
     **那么,用户没有安装一个互斥对象实现通过sqlite3_config () 
     **sqlite3_initialize()前被调用。
-   ** 这段复制指针向sqlite3GlobalConfig默认实现结构。
+    **这段复制指针向sqlite3GlobalConfig默认实现结构。
     */
-/////*如果xMutexAlloc 函数没被设置（还没分配互斥锁），则用户没有安装一个经由先验sqlite3_config()到
-////   sqlite3_initialize()的互斥的实现。
-////  这一块的副本的指针的默认实现为sqlite3globalconfig结构
-*/
     sqlite3_mutex_methods const *pFrom;
     sqlite3_mutex_methods *pTo = &sqlite3GlobalConfig.mutex;
 
-    if( sqlite3GlobalConfig.bCoreMutex ){   //// 启用核心互斥锁
-      pFrom = sqlite3DefaultMutex();///// 为多线程调用   sqlite3_mutex_methods const *sqlite3DefaultMutex(void);
+    if( sqlite3GlobalConfig.bCoreMutex ){
+      pFrom = sqlite3DefaultMutex();
     }else{
-      pFrom = sqlite3NoopMutex();///// 为单线程调用   sqlite3_mutex_methods const *sqlite3NoopMutex(void);
+      pFrom = sqlite3NoopMutex();
     }
-    memcpy(pTo, pFrom, offsetof(sqlite3_mutex_methods, xMutexAlloc));/////将偏移量为offsetof(sqlite3_mutex_methods, xMutexAlloc)的pFrom赋给pTo的开始部分    xMutexAlloc为一个互斥锁的地址
+    memcpy(pTo, pFrom, offsetof(sqlite3_mutex_methods, xMutexAlloc));
     memcpy(&pTo->xMutexFree, &pFrom->xMutexFree,
            sizeof(*pTo) - offsetof(sqlite3_mutex_methods, xMutexFree));
     pTo->xMutexAlloc = pFrom->xMutexAlloc;
   }
-  rc = sqlite3GlobalConfig.mutex.xMutexInit();//////*The xMutexInit method defined by this structure is invoked
-/////// as part of system initialization by the sqlite3_initialize() function. 
-///////The xMutexInit routine is called by SQLite exactly once for each effective call to sqlite3_initialize().
-//////xMutexInit方法是被sqlite3_initialize()系统初始化的一部分。当每个有效的调用sqlite3_initialize()时调用xMutexInit方法
-*/
+  rc = sqlite3GlobalConfig.mutex.xMutexInit();
 
 #ifdef SQLITE_DEBUG
   GLOBAL(int, mutexIsInit) = 1;
@@ -84,15 +71,12 @@ int sqlite3MutexInit(void){
 /*
 ** Shutdown the mutex system. This call frees resources allocated by
 ** sqlite3MutexInit().
-**关闭互斥系统。释放sqlite3MutexInit()分配的资源。
+** 关闭互斥系统。释放sqlite3MutexInit()分配的资源。
 */
 int sqlite3MutexEnd(void){
   int rc = SQLITE_OK;
   if( sqlite3GlobalConfig.mutex.xMutexEnd ){
-    rc = sqlite3GlobalConfig.mutex.xMutexEnd();///////*The implementation of this method is expected to release all outstanding resources obtained by the mutex methods 
-						//////implementation, especially those obtained by the xMutexInit method.
-						//////该方法的实现是为了释放所有被互斥方法实现获得的资源，尤其是被xMutexInit（）方法获得的资源。
-						*/
+    rc = sqlite3GlobalConfig.mutex.xMutexEnd();
   }
 
 #ifdef SQLITE_DEBUG
@@ -106,7 +90,6 @@ int sqlite3MutexEnd(void){
 ** Retrieve a pointer to a static mutex or allocate a new dynamic one.
 **获取一个指向静态互斥锁或分配一个新的动态互斥锁。
 */
-//////*重新为静态互斥锁获取一个指针，或者分配一个新歌动态互斥锁
 sqlite3_mutex *sqlite3_mutex_alloc(int id){
 #ifndef SQLITE_OMIT_AUTOINIT
   if( sqlite3_initialize() ) return 0;
@@ -138,10 +121,6 @@ void sqlite3_mutex_free(sqlite3_mutex *p){
 **如果其他线程已经拥有互斥锁,
 **那么就将其阻塞,直到它可以获得。
 */
-////*获取互斥锁p
-//// 如果其他线程已经拥有该互斥锁，则将其阻塞，直到其他线程将该锁释放
-////  且请求该锁的优先级较高才能获得该锁
-*/
 void sqlite3_mutex_enter(sqlite3_mutex *p){
   if( p ){
     sqlite3GlobalConfig.mutex.xMutexEnter(p);
@@ -154,9 +133,6 @@ void sqlite3_mutex_enter(sqlite3_mutex *p){
 **获得互斥锁p。如果成功,返回SQLITE_OK。
 **否则,如果另一个线程持有互斥锁,它不能得到,就返回SQLITE_BUSY。
 */
-////*获取互斥锁p
-//// 如果能获得该锁，则返回SQLITE_OK。
-//// 否则，如果其他线程持有该锁，则该请求者不能获得该锁，返回SQLITE_BUSY
 int sqlite3_mutex_try(sqlite3_mutex *p){
   int rc = SQLITE_OK;
   if( p ){
@@ -175,10 +151,6 @@ int sqlite3_mutex_try(sqlite3_mutex *p){
 **如果一个空指针作为参数传递给该函数，那么就不做任何操作
 **
 */
-////*sqlite3_mutex_leave()函数释放一个互斥锁，该锁之前有同一个线程引入的。
-//// 如果该互斥锁不是当前输入的，则这个行为是没定义的。
-//// 如果传入的参数p为空指针，则该函数不做任何操作
-*/
 void sqlite3_mutex_leave(sqlite3_mutex *p){
   if( p ){
     sqlite3GlobalConfig.mutex.xMutexLeave(p);
@@ -192,14 +164,9 @@ void sqlite3_mutex_leave(sqlite3_mutex *p){
 **sqlite3_mutex_held() 和sqlite3_mutex_notheld() 函数用于内部assert() 语句
 **
 */
-////* 当持有互斥锁时，返回 p==0 或 sqlite3GlobalConfig.mutex.xMutexHeld(p);
-*/
 int sqlite3_mutex_held(sqlite3_mutex *p){
   return p==0 || sqlite3GlobalConfig.mutex.xMutexHeld(p);
 }
-
-////* 当不持有互斥锁时，返回 p==0 或 sqlite3GlobalConfig.mutex.xMutexNotheld(p);
-*/
 int sqlite3_mutex_notheld(sqlite3_mutex *p){
   return p==0 || sqlite3GlobalConfig.mutex.xMutexNotheld(p);
 }
